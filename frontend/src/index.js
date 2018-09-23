6e5e4f597a8f4a2c22352242c406dbbc1d9c798e
David Johnston <david@blacksheepcode.com>
2018-09-23 16:03:52 +1000

    update git ignore - so I'm not putting my google cloud project ids

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App/App';
-import registerServiceWorker from './registerServiceWorker';
-import { allReducers } from './actions/reducers/reducers';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import { Provider } from 'react-redux'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App/App";
+import registerServiceWorker from "./registerServiceWorker";
+import { allReducers } from "./actions/reducers/reducers";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import LogRocket from "logrocket";
+import config from "./config.json";
+LogRocket.init(config.logrocket.id);
 
-const store = createStore(
-    allReducers,
-    applyMiddleware(thunk)
-);
+const store = createStore(allReducers, applyMiddleware(thunk));
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>, document.getElementById('root'));
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
